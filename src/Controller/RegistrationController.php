c396e4b6d1b3fc02fc5d6afc854bc367f76fee34
AlxFrst <alexandreforestier1107@gmail.com>
2023-11-26 01:44:37 +0000

    Add user registration functionality

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -9,10 +9,43 @@ use Symfony\Component\Routing\Annotation\Route;
 class RegistrationController extends AbstractController
 {
     #[Route('/registration', name: 'app_registration')]
-    public function index(): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('registration/index.html.twig', [
-            'controller_name' => 'RegistrationController',
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+            $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $user->setLinkUserPicture('default.jpg');
+            $user->setRoles(["ROLE_USER"]);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // generate a signed url and email it to the user
+            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+                (new TemplatedEmail())
+                    ->from(new Address('alexandreforestier1107@gmail.com', 'SnowTricks'))
+                    ->to($user->getEmail())
+                    ->subject('Merci de confirmer votre email')
+                    ->htmlTemplate('registration/confirmation_email.html.twig')
+            );
+            // do anything else you need here, like send an email
+            $this->addFlash(
+                'notice',
+                "Votre compte a bien été créé, vous pouvez vous connecter !"
+            );
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
