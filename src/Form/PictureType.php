91e0bc11b9eeabb797f52e75ee59d3a7dce28ae4
AlxFrst <alexandreforestier1107@gmail.com>
2024-01-01 23:17:16 +0000

    Add PictureType form and picture form template

---
+++ src/Form/PictureType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Pictures;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class PictureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('file', FileType::class,[
+                'label' => "Téléverser une image",
+                'multiple' => false,
+                'mapped' => true,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2048k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                        ],
+                        'mimeTypesMessage' => 'Merci de choisir un fichier jpg',
+                    ])
+            ]])
+            ->add('pictureName',TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Pictures::class,
+        ]);
+    }
+}
