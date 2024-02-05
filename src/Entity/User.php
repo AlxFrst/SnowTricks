549aa026cfc44f865fcb94363140b62c3028bc2e
AlxFrst <alexandreforestier1107@gmail.com>
2024-02-05 03:17:45 +0000

    Update composer.json

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+#[UniqueEntity(fields: ['username'], message: 'Il existe déjà un compte avec ce nom d\'utilisateur')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -189,7 +189,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function removePicture(Pictures $picture): self
     {
         if ($this->pictures->removeElement($picture)) {
-            // set the owning side to null (unless already changed)
             if ($picture->getUser() === $this) {
                 $picture->getUser(null);
             }
@@ -219,7 +218,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function removeVideo(Video $video): self
     {
         if ($this->videos->removeElement($video)) {
-            // set the owning side to null (unless already changed)
             if ($video->getUser() === $this) {
                 $video->setUser(null);
             }
